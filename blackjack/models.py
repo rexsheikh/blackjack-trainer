93c28b8b87c16579ca4ac456da36aa397bfbef3c
Rex Sheikh <rex.sheikh@gmail.com>
2023-11-21 17:04:02 -1000

    started finalProject with a single app called blackjack. configured settings, urls and created models with first migration.

---
+++ blackjack/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    rank = models.IntegerField(default=1)
+    cash = models.IntegerField(default=2000)
+
+
+class Hand(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dealer_up = models.IntegerField()
+    player_a = models.IntegerField()
+    player_b = models.IntegerField()
