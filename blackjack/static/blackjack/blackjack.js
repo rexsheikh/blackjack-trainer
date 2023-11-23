fbd3b14b68601fd013acc984b967e109a16e3c20
Rex Sheikh <rex.sheikh@gmail.com>
2023-11-22 14:34:15 -1000

    added add chips to bet and deal functions that mimic basic functionality of adding chips to create a total bet value and deal which mimimics a dealer passing cards to a user.

--- blackjack/static/blackjack/blackjack.js
+++ blackjack/static/blackjack/blackjack.js
@@ -1,3 +1,560 @@
+const stratHardTotal = {
+    5: {
+        2: "hit",
+        3: "hit",
+        4: "hit",
+        5: "hit",
+        6: "hit",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    6: {
+        2: "hit",
+        3: "hit",
+        4: "hit",
+        5: "hit",
+        6: "hit",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    7: {
+        2: "hit",
+        3: "hit",
+        4: "hit",
+        5: "hit",
+        6: "hit",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    8: {
+        2: "hit",
+        3: "hit",
+        4: "hit",
+        5: "hit",
+        6: "hit",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    9: {
+        2: "hit",
+        3: "double",
+        4: "double",
+        5: "double",
+        6: "double",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    10: {
+        2: "double",
+        3: "double",
+        4: "double",
+        5: "double",
+        6: "double",
+        7: "double",
+        8: "double",
+        9: "double",
+        10: "hit",
+        "ace": "hit"
+    },
+    11: {
+        2: "double",
+        3: "double",
+        4: "double",
+        5: "double",
+        6: "double",
+        7: "double",
+        8: "double",
+        9: "double",
+        10: "double",
+        "ace": "double"
+    },
+    12: {
+        2: "hit",
+        3: "hit",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    13: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    14: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    15: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    16: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    17: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "stand",
+        8: "stand",
+        9: "stand",
+        10: "stand",
+        "ace": "stand"
+    },
+    18: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "stand",
+        8: "stand",
+        9: "stand",
+        10: "stand",
+        "ace": "stand"
+    },
+    19: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "stand",
+        8: "stand",
+        9: "stand",
+        10: "stand",
+        "ace": "stand"
+    },
+    20: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "stand",
+        8: "stand",
+        9: "stand",
+        10: "stand",
+        "ace": "stand"
+    },
+};
+
+const stratSoftTotal = {
+    5: {
+        2: "hit",
+        3: "hit",
+        4: "hit",
+        5: "hit",
+        6: "hit",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    6: {
+        2: "hit",
+        3: "hit",
+        4: "hit",
+        5: "hit",
+        6: "hit",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    7: {
+        2: "hit",
+        3: "hit",
+        4: "hit",
+        5: "hit",
+        6: "hit",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    8: {
+        2: "hit",
+        3: "hit",
+        4: "hit",
+        5: "hit",
+        6: "hit",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    9: {
+        2: "hit",
+        3: "double",
+        4: "double",
+        5: "double",
+        6: "double",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    10: {
+        2: "double",
+        3: "double",
+        4: "double",
+        5: "double",
+        6: "double",
+        7: "double",
+        8: "double",
+        9: "double",
+        10: "hit",
+        "ace": "hit"
+    },
+    11: {
+        2: "double",
+        3: "double",
+        4: "double",
+        5: "double",
+        6: "double",
+        7: "double",
+        8: "double",
+        9: "double",
+        10: "double",
+        "ace": "double"
+    },
+    12: {
+        2: "hit",
+        3: "hit",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    13: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    14: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    15: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    16: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "hit",
+        8: "hit",
+        9: "hit",
+        10: "hit",
+        "ace": "hit"
+    },
+    17: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "stand",
+        8: "stand",
+        9: "stand",
+        10: "stand",
+        "ace": "stand"
+    },
+    18: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "stand",
+        8: "stand",
+        9: "stand",
+        10: "stand",
+        "ace": "stand"
+    },
+    19: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "stand",
+        8: "stand",
+        9: "stand",
+        10: "stand",
+        "ace": "stand"
+    },
+    20: {
+        2: "stand",
+        3: "stand",
+        4: "stand",
+        5: "stand",
+        6: "stand",
+        7: "stand",
+        8: "stand",
+        9: "stand",
+        10: "stand",
+        "ace": "stand"
+    },
+};
+
+const stratPairs = {
+    2: {
+        2: "y",
+        3: "y",
+        4: "y",
+        5: "y",
+        6: "y",
+        7: "y",
+        8: "n",
+        9: "n",
+        10: "n",
+        "ace": "n",
+    },
+    3: {
+        2: "y",
+        3: "y",
+        4: "y",
+        5: "y",
+        6: "y",
+        7: "y",
+        8: "n",
+        9: "n",
+        10: "n",
+        "ace": "n",
+    },
+    4: {
+        2: "n",
+        3: "n",
+        4: "n",
+        5: "y",
+        6: "y",
+        7: "n",
+        8: "n",
+        9: "n",
+        10: "n",
+        "ace": "n",
+    },
+    5: {
+        2: "n",
+        3: "n",
+        4: "n",
+        5: "n",
+        6: "n",
+        7: "n",
+        8: "n",
+        9: "n",
+        10: "n",
+        "ace": "n",
+    },
+    6: {
+        2: "y",
+        3: "y",
+        4: "y",
+        5: "y",
+        6: "y",
+        7: "n",
+        8: "n",
+        9: "n",
+        10: "n",
+        "ace": "n",
+    },
+    7: {
+        2: "y",
+        3: "y",
+        4: "y",
+        5: "y",
+        6: "y",
+        7: "y",
+        8: "n",
+        9: "n",
+        10: "n",
+        "ace": "n",
+    },
+    8: {
+        2: "y",
+        3: "y",
+        4: "y",
+        5: "y",
+        6: "y",
+        7: "y",
+        8: "y",
+        9: "y",
+        10: "y",
+        "ace": "y",
+    },
+    9: {
+        2: "y",
+        3: "y",
+        4: "y",
+        5: "y",
+        6: "y",
+        7: "n",
+        8: "y",
+        9: "y",
+        10: "n",
+        "ace": "n",
+    },
+    10: {
+        2: "y",
+        3: "y",
+        4: "y",
+        5: "y",
+        6: "y",
+        7: "y",
+        8: "n",
+        9: "n",
+        10: "n",
+        "ace": "n",
+    },
+    "ace": {
+        2: "y",
+        3: "y",
+        4: "y",
+        5: "y",
+        6: "y",
+        7: "y",
+        8: "y",
+        9: "y",
+        10: "y",
+        "ace": "y",
+    },
+};
+
+const cards = [2,3,4,5,6,7,8,9,10,11];
+let totalBet = 0; 
+
 document.addEventListener('DOMContentLoaded',function(){
-    console.log('hello world')
+    console.log('dom loaded...');
+    document.getElementById('deal-view').style.display = 'none';
+    addChipToBet();
+    deal();
 })
+
+
+function addChipToBet(){
+    const betBtns = document.querySelectorAll('.bet-btn');
+    betBtns.forEach(function(btn){
+        btn.addEventListener('click',function(){
+            const betVal = parseInt(btn.getAttribute('data-chip-val'),10);
+            totalBet += betVal;
+            console.log(totalBet);
+        })
+    })
+}
+
+function deal(){
+    document.querySelector('.deal-btn').addEventListener('click',function(){
+        document.querySelector('#bet-view').style.display = 'none';
+    })
+    let dealerUp = getRandomCard();
+    let player_card_a = getRandomCard();
+    let player_card_b = getRandomCard();
+    const dealerShow = document.createElement('h2');
+    const playerCardShowA = document.createElement('h2');
+    const playerCardShowB = document.createElement('h2');
+    dealerShow.innerHTML = dealerUp;
+    playerCardShowA.innerHTML = player_card_a;
+    playerCardShowB.innerHTML = player_card_b;
+    document.getElementById('dealer-cards').appendChild(dealerShow);
+    document.getElementById('player-cards').appendChild(playerCardShowA);
+    document.getElementById('player-cards').appendChild(playerCardShowB);
+    document.getElementById('deal-view').style.display = 'block';
+
+    
+}
+
+function getRandomCard(){
+    let idx = Math.floor(Math.random() * cards.length);
+    return cards[idx];
+}
